a7f5005e6747d3c98b433a48758cd0af9d9e3cb2
luizk <luiz.kressin@gmail.com>
2021-03-18 15:46:04 -0300

    read rx

---
+++ read_rx.py
@@ -0,0 +1,18 @@
+import serial
+
+ser = serial.Serial("/dev/ttyS0", baudrate=2000000)
+readings = []
+done = False
+while not done:
+    current_char = ser.read()
+    print(current_char)
+    """ # check for equals sign
+    if current_char == b'=':
+        reading = ser.read(8)
+        readings.append(reading)
+
+    # this part will depend on your specific needs.
+    # in this example, we stop after 10 readings
+    # check for stopping condition and set done = True
+    if len(readings) >= 10:
+        done = True """
